f7d294a953d0be94695c60205fba5241c2b66359
Billy Lam <0chun0@gmail.com>
2019-05-15 18:09:20 +0800

    Test db pool method

--- R/connection.R
+++ R/connection.R
@@ -1,12 +1,18 @@
-pool <- pool::dbPool(
-  #drv = RPostgreSQL::PostgreSQL(),
-  drv = dbDriver("PostgreSQL"),
-  dbname = "stock",
-  host = "127.0.0.1",
-  user = "postgres",
-  password = "P@ssw0rDB",
-  #password = "",
-  maxSize = 10,
-  idleTimeout = 120
+get_connection <- function(){
+	pool <- pool::dbPool(
+
+	  drv = RPostgreSQL::PostgreSQL(),
+	  dbname = "stock",
+	  host = "127.0.0.1",
+	  user = "db_user",
+	  password = "P@ssw0rDB",
+	  #password = "",
+	  maxSize = 10,
+	  idleTimeout = 120
+
+	)
+	pool
 
 }
+
+# DBI::dbDisconnect(pool
