6531b97da99ebe1bc0080b8cfe396c874d2e3a73
Billy.Lam <billyla@aswatson.com>
2019-06-04 06:31:21 +0800

    Add local or remote connections

--- R/connection.R
+++ R/connection.R
@@ -1,24 +1,37 @@
-sql_connection <- function(){
+sql_connection <- function(local = FALSE){
 
   ## Return connection for remote database
+  if(local == TRUE){
 
-  conn <- DBI::dbConnect(drv    = RPostgreSQL::PostgreSQL(),
-                         dbname = "stock",
-                         host   = "206.189.149.240",
-                         port   = 4004,
-                         user   = "db_user",
-                         password = 'P@ssw0rDB')
+    conn <- DBI::dbConnect(drv    = RPostgreSQL::PostgreSQL(),
+                           dbname = "stock",
+                           host   = "localhost",
+                           port   = 4004,
+                           user   = "db_user",
+                           password = 'P@ssw0rDB')
+
+  } else{
+    # Remote
+    conn <- DBI::dbConnect(drv    = RPostgreSQL::PostgreSQL(),
+                           dbname = "stock",
+                           host   = "206.189.149.240",
+                           port   = 4004,
+                           user   = "db_user",
+                           password = 'P@ssw0rDB')
+
+  }
+
   return(conn)
 }
 
 
-sql_query <- function(sql){
+sql_query <- function(sql, local = FALSE){
 
   ### Get data from the provided sql
 
-  conn <- sql_connection()
+  conn <- sql_connection(connect)
 
-  res <- data.table(DBI::dbGetQuery(conn, sql))
+  res <- data.table::as.data.table(DBI::dbGetQuery(conn, sql))
 
   DBI::dbDisconnect(conn)
 
