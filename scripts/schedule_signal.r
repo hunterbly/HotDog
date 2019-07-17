a297a1b82eb05c37b594f4bdfc31ee257ebbbc65
Billy Lam <0chun0@gmail.com>
2019-07-17 14:43:42 +0800

    Change namespace export

--- scripts/schedule_signal.r
+++ scripts/schedule_signal.r
@@ -5,7 +5,9 @@ library(HotDog)
 args = commandArgs(trailingOnly=TRUE)
 
 input.date = args[1]
-
+str(input.date)
 # Call
-df = get_hit_signal(ref.date = date, format = 'long', local = TRUE)
-return(df)
+# df = get_hit_signal(ref.date = date, format = 'long', local = TRUE)
+# return(df)
+df = get_hit_signal(ref.date = input.date, format = 'long', local = TRUE)
+print(nrow(df))
