a0dadfa2f13e2e8392dd8ad48207485355d1412b
0chun0 <0chun0@gmail.com>
2020-01-25 08:35:50 +0800

    Add dataframe

--- R/util.R
+++ R/util.R
@@ -35,29 +35,50 @@ round_dataframe <- function(df, digits = 2){
   return(df)
 }
 
-check_cronjob <-function(){
+check_cronjob <-function(local = FALSE){
 
-  sql_stock = 'SELECT DATE, COUNT(1)
+  ####
+  # SQL for checking
+  ####
+
+  sql.stock = 'SELECT DATE, COUNT(1)
                 FROM STOCK
                 GROUP BY DATE
                 ORDER BY DATE DESC
                 LIMIT 10'
 
-  sql_ccass = 'SELECT DATE, COUNT(1)
+  sql.ccass = 'SELECT DATE, COUNT(1)
               FROM CCASS
               GROUP BY DATE
               ORDER BY DATE DESC
               LIMIT 10'
 
-  sql_option = 'SELECT DATE, COUNT(1)
+  sql.option = 'SELECT DATE, COUNT(1)
                 FROM OPTION
                 GROUP BY DATE
                 ORDER BY DATE DESC
                 LIMIT 10'
-  sql_signal = 'SELECT *
-              FROM SIGNAL_HISTORY
-              ORDER BY DATE DESC
-              LIMIT 10'
+
+  sql.signal = 'SELECT DATE, COUNT(1)
+                FROM SIGNAL_HISTORY
+                GROUP BY DATE
+                ORDER BY DATE DESC
+                LIMIT 10'
+
+  ####
+  # Result dataframe
+  ####
+
+  df.stock  = sql_query(sql.stock,  local)
+  df.ccass  = sql_query(sql.ccass,  local)
+  df.option = sql_query(sql.option, local)
+  df.signal = sql_query(sql.signal, local)
 
 
+  print(head(df.stock))
+  print(head(df.ccass))
+  print(head(df.option))
+  print(head(df.signal))
+
+  return(df.stock)
 }
