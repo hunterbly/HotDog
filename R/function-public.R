e135eea81831515adb33750f8369be5d13a233a2
Billy Lam <0chun0@gmail.com>
2020-02-04 09:14:20 +0800

    Change back to dot format

--- R/function-public.R
+++ R/function-public.R
@@ -2,14 +2,10 @@ GetSignalPerformance <- function(){
     return("ABC")
 }
 
-LoadHitSignal <- function(ref_date, option_only = TRUE, local = TRUE){
-
-    # Variable name conversion
-    ref.date = ref_date
-    option.only = option_only
+LoadHitSignal <- function(ref.date, option.only = TRUE, local = TRUE){
 
     # Selected column only
-    df.sign     al = load_hit_signal(ref.date, format = 'long', option.only, local)
+    df.signal = load_hit_signal(ref.date, format = 'long', option.only, local)
     df.signal.selected = df.signal[, c('code', 'date', 'signal'), with=FALSE]
 
     return(df.signal.selected)
