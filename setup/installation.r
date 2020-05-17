67320a321bd5c351245cb22878e376215627aa05
Billy Lam <BillyLa@aswatson.com>
2020-05-17 21:19:16 +0800

    Add opencpu host server script as well

--- setup/installation.r
+++ setup/installation.r
@@ -1,2 +1,5 @@
 remove.packages('HotDog')
 devtools::install(pkg = ".", quick = TRUE, dependencies = FALSE, upgrade = "never", keep_source = TRUE)
+
+# Host server
+opencpu::ocpu_start_server(port = 4000)
