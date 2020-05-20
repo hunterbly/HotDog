7d323d832d7e2a4251abdd4cb615d1d8621564ce
Billy Lam <0chun0@gmail.com>
2020-05-20 08:51:07 +0800

    Add column to public function

--- R/function-public.R
+++ R/function-public.R
@@ -1,16 +1,16 @@
 GetSignalPerformance <- function(code, local = TRUE){
 
     df.performance = get_signal_performance(code, local)
-    
+
     # Get selected columns
     col.all = colnames(df.performance)
     col.price = col.all[grepl("day\\.\\d*$", col.all, perl=TRUE)]
     col.selected = do.call(c, list(c('date', 'code', 'signal', 'signal_index'),
                                col.price,
                                c('success')))
-        
+
     df.res = df.performance[, col.selected, with = FALSE][order(code, signal, -date)]
-    
+
     return(df.res)
 }
 
@@ -18,7 +18,7 @@ LoadHitSignal <- function(ref.date, option.only = TRUE, local = TRUE){
 
     # Selected column only
     df.signal = load_hit_signal(ref.date, format = 'long', option.only, local)
-    df.signal.selected = df.signal[, c('code', 'date', 'signal'), with=FALSE]
+    df.signal.selected = df.signal[, c('code', 'date', 'signal', 'signal_index'), with=FALSE]
 
     return(df.signal.selected)
 }
